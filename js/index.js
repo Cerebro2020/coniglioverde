4ce7a8f858be2fd25e1a5400a92a97099be0d51f
Felice Vino <65618719+Cerebro2020@users.noreply.github.com>
2025-09-12 13:37:06 +0200

    Update index.js
    update rotation

--- js/index.js
+++ js/index.js
@@ -24,6 +24,7 @@ export default function(){
   const camera = new THREE.PerspectiveCamera(50, window.innerWidth/window.innerHeight, 0.1, 10000);
   const player = { height:1.8, speed:0.2, turnSpeed:Math.PI*0.02 };
   camera.position.set(0, 0, 0);
+  camera.rotation.set(0,0,0);
   camera.lookAt(new THREE.Vector3(0, player.height, 0));
   camera.setFocalLength(35);
 
